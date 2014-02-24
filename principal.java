4eb4af054aee2da21966b153289ec32c8b901e0a
Javi <correo.javi@gmail.com>
2014-02-24 21:33:13 +0100

    He cambiat el valor de pi

--- principal.java
+++ principal.java
@@ -1,6 +1,6 @@
 public class principal {
 
-  public double pi () {return (3.14159);}
+  public double pi () {return (3.1416);}
 
   public static void main (String[] args) {
 	System.out.print("Adios");
